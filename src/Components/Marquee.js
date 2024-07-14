c394929a7414595342312e02aaeb79e5511725a6
Anna <annarosewain9@gmail.com>
2024-07-14 11:51:58 +0100

    Animated marquee using CSS and created state to handle pause on hover and reverse on double click

--- src/Components/Marquee.js
+++ src/Components/Marquee.js
@@ -1,29 +1,29 @@
-import React from "react";
-import "./Styles/App.css";
+import React, { useState } from "react";
+import "../Styles/App.css";
 
 //https://www.freecodecamp.org/news/how-to-build-a-marquee-component-with-react/
 
 export default function Marquee() {
+  const [reverseAnimation, setReverseAnimation] = useState(false);
+  const handleDoubleClick = () => {
+    setReverseAnimation((prev) => !prev);
+  };
   return (
     <div className="marquee-container">
-      <div className="marquee">
-        <ul className="marquee__content">
-          <div className=" marquee__item">
-            <p>React.js</p>
-          </div>
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-        </ul>
-
-        <ul aria-hidden="true" className="marquee__content">
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-          <div className=" marquee__item"></div>
-        </ul>
-      </div>
+      <section className="enable-animation">
+        <div className="marquee" onDoubleClick={handleDoubleClick}>
+          <ul className="marquee__content">
+            <div className="marquee__item">
+              <p>React.js</p>
+            </div>
+          </ul>
+          <ul aria-hidden="true" className="marquee__content">
+            <div className=" marquee__item"></div>
+            <div className=" marquee__item"></div>
+            <div className=" marquee__item"></div>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
