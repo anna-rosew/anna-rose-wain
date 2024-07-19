f5a5c95c08422453c9219f82dcd76bfdbfb78bcc
Anna <annarosewain9@gmail.com>
2024-07-19 11:06:31 +0100

    Added animated icons

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../Styles/App.css";
 import "../Styles/Hero.css";
 import ContactButton from "../Components/ContactButton";
+import lightningIcon from "";
 
 const spotlightSize = 200; // Define the size of the spotlight
 
@@ -31,12 +32,10 @@ export default function Hero() {
       <div className="spotlight" style={spotlightStyle}></div>
       <div className="hero">
         <h2 className="bio">
-          <strong>
-            I’m Anna-Rose, I’m a junior front-end developer who specializes in
-            creating interactive custom websites. With a background in the arts,
-            I love using javascript creatively to make unique user interfaces
-            using clean code.
-          </strong>{" "}
+          I’m Anna-Rose, a <img src={lightningIcon} alt="Lightning icon"></img>
+          junior front-end developer who specializes in creating interactive
+          custom websites. With a background in the arts, I love using
+          javascript creatively to make unique user interfaces using clean code.
           <br />
           <br />
           Inclusivity and sustainability on the web are also very important to
