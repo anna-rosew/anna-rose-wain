d6641e65b5b539891390e57292fb68d4b13f2642
Anna <annarosewain9@gmail.com>
2024-07-21 15:07:47 +0100

    Fixing babel error

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Styles/App.css";
 import "../Styles/Header.css";
-import { Link } from "react-scroll";
+//import { Link } from "react-scroll";
 
 export default function Header() {
   return (
@@ -10,18 +10,18 @@ export default function Header() {
       <header>
         <div className="header-item 1">switch it up</div>
         <div className="header-item 2">Anna-Rose Wain</div>
-        <div className="header-item 3">
-          <Link
-            to="work"
-            smooth={true}
-            duration={500}
-            alt="Work"
-            className="work-anchor"
-          >
-            Work
-          </Link>
-        </div>
+        <div className="header-item 3">Work</div>
       </header>
     </div>
   );
 }
+
+//<Link
+//to="work"
+//smooth={true}
+//duration={500}
+//alt="Work"
+//className="work-anchor"
+//>
+// Work
+//</Link>
