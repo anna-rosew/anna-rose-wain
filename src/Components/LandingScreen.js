3c0a0a9bb971404e1ae832d00d0a2a1e776dd96c
Anna <annarosewain9@gmail.com>
2024-07-24 21:15:10 +0100

    Uninstall gsap because of pay wall

--- src/Components/LandingScreen.js
+++ src/Components/LandingScreen.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../Styles/LandingScreen.css";
 import Header from "./Header";
-import { gsap } from "gsap";
-import { TextPlugin } from "gsap/TextPlugin";
+
 
 import emoji from "../imgs/icons/robot.svg";
 import arrow from "../imgs/icons/arrow.svg";
@@ -17,8 +16,11 @@ export default function LandingScreen() {
       </div>
       <h1>CREATIVE</h1>
       <h1>DEVELOPER/DESIGNER</h1>
-      <div className="text-container" id="quote">
-        I’M A JUNIOR FRONT-END DEVELOPER SPECIALISING IN INTERACTIVE INTERFACES.
+      <div className="text-container">
+        <h2>
+          I’M A JUNIOR FRONT-END DEVELOPER SPECIALISING IN INTERACTIVE
+          INTERFACES.{" "}
+        </h2>
       </div>
       <div className="arrow">
         <img src={arrow} alt="Arrow pointing down" />
@@ -26,5 +28,3 @@ export default function LandingScreen() {
     </div>
   );
 }
-
-gsap.registerPlugin(TextPlugin);
