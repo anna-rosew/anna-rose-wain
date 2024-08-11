f4af59d80e6b17fa02dbce75363ece99caa5ec03
Anna <annarosewain9@gmail.com>
2024-08-11 13:34:48 +0100

    More bio editing

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,47 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../Styles/App.css";
 import "../Styles/Hero.css";
 
+import ContactButton from "../Components/ContactButton";
+
 import lightning from "../imgs/icons/lightning.svg";
 
-const spotlightSize = 200; // Define the size of the spotlight
-
 export default function Hero() {
-  const [spotlightStyle, setSpotlightStyle] = useState({
-    left: "50%",
-    top: "50%",
-  });
-
-  const handleMouseMove = (event) => {
-    const { clientX: x, clientY: y } = event;
-    setSpotlightStyle({
-      left: `${x - spotlightSize / 2}px`,
-      top: `${y - spotlightSize / 2}px`,
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
   return (
     <div className="hero-container" id="about">
-      <div className="spotlight" style={spotlightStyle}></div>
-      <div className="hero">
-        <p className="bio">
-          {" "}
-          <span className="line">
-            <span>I’m Anna-Rose, I’m a</span>
-            <span className="show-gif-on-hover">
-              <svg src={lightning} alt="Lightning icon wiggling."></svg>
-            </span>
+      <p className="bio">
+        <span className="line">
+          <span>I’m Anna-Rose, I’m a</span>
+          <span className="show-gif-on-hover">
+            <img src={lightning} alt="Lightning icon wiggling." />
           </span>
-        </p>
-      </div>
-      <hr></hr>
+          <span>web developer</span>
+          <span className="show-gif-on-hover">
+            <img src={lightning} alt="Lightning icon wiggling." />
+          </span>
+        </span>
+      </p>
+      <ContactButton />
     </div>
   );
 }
