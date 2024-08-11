a6c6e65a68f7e71be6e1be66e8be330bdb6f8eb0
Anna <annarosewain9@gmail.com>
2024-08-11 13:08:04 +0100

    Editing bio

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "../Styles/App.css";
 import "../Styles/Hero.css";
-import ContactButton from "../Components/ContactButton";
+
+import lightning from "../imgs/icons/";
 
 const spotlightSize = 200; // Define the size of the spotlight
 
@@ -30,97 +31,15 @@ export default function Hero() {
     <div className="hero-container" id="about">
       <div className="spotlight" style={spotlightStyle}></div>
       <div className="hero">
-        <h2 className="bio">
-          <strong>
-            I’m Anna-Rose, I’m a junior front-end developer who specializes in
-            creating interactive custom websites. With a background in the arts,
-            I love using javascript creatively to make unique user interfaces
-            using clean code.
-          </strong>{" "}
-          <br />
-          <br />
-          Inclusivity and sustainability on the web are also very important to
-          me. When I’m not neck deep in debugging, you’ll probably find me
-          running up a hill somewhere.
-          <br />
-          <br />I love chatting about code, design and all things collaboration
-          so say hello! ☻
-        </h2>
-        <div className="details-container">
-          <div className="row justify-content-start left-align">
-            <div className="col-sm-6 col-md">
-              <div className="summary-links">
-                <h2>Get in touch</h2>
-                <a
-                  className="summary"
-                  href="mailto:annarosewain9@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                  alt="Email me"
-                >
-                  Email Me
-                </a>
-              </div>
-            </div>
-            <div className="col-sm-6 col-md">
-              <div className="summary-links">
-                <h2>GitHub</h2>
-                <a
-                  className="summary"
-                  href="https://github.com/anna-rosew"
-                  target="_blank"
-                  rel="noreferrer"
-                  alt="Link to GitHub Account"
-                >
-                  /anna-rosew
-                </a>
-              </div>
-            </div>
-            <div className="col-sm-6 col-md">
-              <div className="summary-links">
-                <h2>LinkedIn</h2>
-                <a
-                  className="summary"
-                  href="https://www.linkedin.com/in/anna-rose-wain/"
-                  target="_blank"
-                  rel="noreferrer"
-                  alt="Link to LinkedIn account"
-                >
-                  /anna-rose-wain
-                </a>
-              </div>
-            </div>
-            <div className="col-sm-6 col-md">
-              <div className="summary-links">
-                <h2>SheCodes</h2>
-                <a
-                  className="summary"
-                  href="https://www.shecodes.io/graduates/68516-anna-rose-wain"
-                  target="_blank"
-                  rel="noreferrer"
-                  alt="Link to SheCodes account"
-                >
-                  Qualifications
-                </a>
-              </div>
-            </div>
-            <div className="col-sm-6 col-md">
-              <div className="summary-links">
-                <h2>CV</h2>
-                <a
-                  className="summary"
-                  href="https://www.dropbox.com/scl/fi/4zxhf8sii29vgpqh33at7/Anna-Rose-Wain-s-CV-2024.pdf?rlkey=bes7qa9wphqf8do7cbb4klf0n&st=30qiwgwj&dl=0"
-                  target="_blank"
-                  rel="noreferrer"
-                  alt="Link to CV"
-                >
-                  View Online
-                </a>
-              </div>
-            </div>
-          </div>
-          <ContactButton />
-        </div>
+        <p className="bio">
+          {" "}
+          <span className="line">
+            <span>I’m Anna-Rose, I’m a</span>
+            <span className="show-gif-on-hover">
+              <svg src={lightning} alt="Lightning icon wiggling."></svg>
+            </span>
+          </span>
+        </p>
       </div>
       <hr></hr>
     </div>
