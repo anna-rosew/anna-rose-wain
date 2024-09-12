78f8e44743f3a001a0744851051425237709a767
Anna <annarosewain9@gmail.com>
2024-09-12 15:14:14 +0100

    Creating the bio para

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -5,21 +5,24 @@ import "../Styles/Bio.css";
 import ContactButton from "../Components/ContactButton";
 
 import lightning from "../imgs/icons/lightning.svg";
-import internet from "../imgs/icons/lightning.svg";
+import internet from "../imgs/icons/internet.svg";
 
 export default function Bio() {
   return (
     <div className="bio-container" id="about">
       <p className="bio">
         <span className="line">
-          <span>I’m Anna-Rose, I’m a</span>
-          <span className="show-gif-on-hover">
-            <img src={lightning} alt="Lightning icon wiggling." />
-          </span>
-          <span>web developer</span>
-          <span className="show-gif-on-hover">
-            <img src={internet} alt="Lightning icon wiggling." />
-          </span>
+          <div>
+            <span>I’m Anna-Rose, </span>
+            <span className="show-gif-on-hover invert">
+              <img src={lightning} alt="Lightning icon wiggling." />
+            </span>
+            <span>web developer</span>
+            <span className="show-gif-on-hover">
+              <img src={internet} alt="Lightning icon wiggling." />
+            </span>
+            <span>who specialises in creating</span>
+          </div>
         </span>
       </p>
       <ContactButton />
